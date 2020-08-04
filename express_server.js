a3030149e8cc7e53b8b13be9b3f83e9c2c5c8b50
Devin Coughlin <devin_coughlin@hotmail.com>
2020-08-04 00:24:47 +0000

    added random number generator funciton for short URLs

--- express_server.js
+++ express_server.js
@@ -7,6 +7,10 @@ app.set("view engine", "ejs")
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 
+function generateRandomString() {
+  return Math.random().toString(36).substring(2,8)
+}
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -40,4 +44,9 @@ app.get("/urls/new", (req, res) => {
 app.get("/urls/:shortURL", (req, res) => {
 	let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]}
   res.render("urls_show", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
