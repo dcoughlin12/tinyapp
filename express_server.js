f0ff7c29400d20ff75706950e6738bf192808dbf
Devin Coughlin <devin_coughlin@hotmail.com>
2020-08-05 04:08:56 +0000

    showing username in header

--- express_server.js
+++ express_server.js
@@ -3,6 +3,9 @@ const app = express();
 const PORT = 8080; // default port 8080
 const bodyParser = require("body-parser");
 const cookieParser = require('cookie-parser')
+
+
+app.use(cookieParser());
 //using express to use ejs as engine
 app.set("view engine", "ejs")
 //converting request body to a string from a buffer. then adding data to req object 
@@ -30,16 +33,17 @@ app.get("/urls.json", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-	let templateVars = { urls: urlDatabase,}
+	let templateVars = { urls: urlDatabase, username: req.cookies.username }
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+	let templateVars = { username: req.cookies.username };
+  res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-	let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]}
+	let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], username: req.cookies.username }
   res.render("urls_show", templateVars);
 });
 
@@ -77,7 +81,8 @@ app.post("/urls/:shortURL", (req, res) => {
 // Redirect the browser back to the /urls page.
 app.post("/login", (req, res) => {
 	res.cookie('username', req.body.login)
-	console.log(req.body.login)// this is the username. 
+	console.log(req.cookies)
+	// console.log(req.body.login)// this is the username. 
 	res.redirect('/urls')
 })
 
