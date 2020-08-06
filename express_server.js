df74d93c5bab8aba48bdf3733667c50d6f7ac30f
Devin Coughlin <devin_coughlin@hotmail.com>
2020-08-06 06:02:52 +0000

    accounts can only see their own urls

--- express_server.js
+++ express_server.js
@@ -46,11 +46,28 @@ const checkIfPasswordMatches = function(userObject, passwordGiven, emailGiven) {
   return false;
 };
 
+//Function that returns all the urls in the database that have the same ID as the logged in user
+// returns an array of the longURL strings from all the objects with matching Id's in the datebase. 
+const urlsForUser = function(id) {
+	const individualLongUrls = {};
+  for (let eachUrlObj in urlDatabase) {
+  	if (urlDatabase[eachUrlObj].userID === id) {
+  		individualLongUrls[eachUrlObj] = urlDatabase[eachUrlObj];
+  	}
+  }
+  return individualLongUrls
+};
+
 //database containing short URLS and long URLS ad values
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  "b2xVn2": { longURL: "http://www.lighthouselabs.ca", userID: "sample" },
+  "9sm5xK": { longURL: "http://www.google.com", userID: "sample2" },
 };
+// const urlDatabase = { OLD DATABASE STRUCTURE ONLY HERE FOR REFERENCE WHEN MAKING CHANGES. DELETE LATER
+//   "b2xVn2": "http://www.lighthouselabs.ca",
+//   "9sm5xK": "http://www.google.com"
+// };
+
 //darabase containing user information
 const users = {
   "userRandomID": {
@@ -64,43 +81,52 @@ const users = {
     password: "dishwasher-funk"
   }
 };
-
+// HOME
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
-
+//PORT
 app.listen(PORT, () => {
   console.log(`Tiny app listening on port ${PORT}!`);
 });
-//shows JSON string of the dtabase
-// app.get("/urls.json", (req, res) => {
-//   res.json(urlDatabase);
-// });
 
+
+//Main page to view URLS
 app.get("/urls", (req, res) => {
-  let templateVars = { urls: urlDatabase, user: users[req.cookies.user_id] };
+	// console.log(urlsForUser(users[req.cookies.user_id].id))
+	const loginAccountId = req.cookies.user_id;
+	const accountUrls = urlsForUser(loginAccountId);
+	console.log(accountUrls)
+  const templateVars = { urls: accountUrls, user: users[loginAccountId] };
   res.render("urls_index", templateVars);
 });
 
+
+//page with input field to enter long URL. if not logged in redirected to loogin page
 app.get("/urls/new", (req, res) => {
   let templateVars = { user: users[req.cookies.user_id] };
-  res.render("urls_new", templateVars);
+  if (templateVars.user) {
+  	res.render("urls_new", templateVars);
+  } else {
+  res.redirect("/login");
+}
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], user: users[req.cookies.user_id] };
+  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL].longURL, user: users[req.cookies.user_id] };
   res.render("urls_show", templateVars);
 });
 
+// post to create the tinyURL
 app.post("/urls", (req, res) => {
   const shortURL = generateRandomString();
-  urlDatabase[shortURL] = req.body.longURL;
+  urlDatabase[shortURL] = { 'longURL': req.body.longURL, 'userID': users[req.cookies.user_id].id };
   res.redirect(`/urls/${shortURL}`);
 });
 
 // Short URL
 app.get("/u/:shortURL", (req, res) => {
-  const longURL = urlDatabase[req.params.shortURL]; //// need to see if [req.params.shortURL] is correct.. dont understand the path
+  const longURL = urlDatabase[req.params.shortURL].longURL; //// need to see if [req.params.shortURL] is correct.. dont understand the path
   res.redirect(longURL);
 });
 
@@ -113,7 +139,7 @@ app.post("/urls/:shortURL/delete", (req, res) => {
 
 // taking in the edit input, updating database to reflect new given long URL
 app.post("/urls/:shortURL", (req, res) => {
-  urlDatabase[req.params.shortURL] = req.body.editedURL;
+  urlDatabase[req.params.shortURL].longURL = req.body.editedURL;
   res.redirect('/urls');
 });
 
@@ -144,14 +170,14 @@ app.post("/register", (req, res) => {
 });
 
 app.get("/login", (req, res) => {
-  let templateVars = { user: users[req.cookies.user_id] };
+  const templateVars = { user: users[req.cookies.user_id] };
   res.render("urls_login", templateVars);
 });
 
 // renders login page when header button is pressed.
 app.post("/login", (req, res) => {
-  let loginEmail = req.body.email;
-  let loginPassword = req.body.password;
+  const loginEmail = req.body.email;
+  const loginPassword = req.body.password;
   if (checkIfEmailExists(users, loginEmail) && checkIfPasswordMatches(users, loginPassword, loginEmail)) {
     const matchedId = fetchUserIdFromDatabase(users, loginEmail);
     res.cookie("user_id", matchedId);
